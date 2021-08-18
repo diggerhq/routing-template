bb060db2d0f87564b3b3e4359ebfce705ab73af0
motatoes <moe.habib9@gmail.com>
2021-08-18 11:31:35 +0300

    support for custom domains

--- main/routing-apigateway.template.tf
+++ main/routing-apigateway.template.tf
@@ -2,7 +2,31 @@
 resource "aws_apigatewayv2_api" "routing" {
   name          = "${var.project_name}-${var.environment}-gateway"
   protocol_type = "HTTP"
+
+  {% if routing_domain %}
+  cors_configuration {
+    allow_credentials  = true 
+    allow_headers      = "*"
+    allow_methods      = "*"
+    allow_origins      = "{{routing_domain}}"
+    # expose_headers     = ""
+    max_age            = 100
+  }
+  {% endif %}
+
 }
+
+{% if routing_domain and routing_certificate %}
+resource "aws_apigatewayv2_domain_name" "domain_{{routing_id}}" {
+  domain_name = "{{routing_domain}}"
+
+  domain_name_configuration {
+    certificate_arn = "{{routing_certificate}}"
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+{% endif %}
 
 resource "aws_apigatewayv2_stage" "routing" {
   api_id      = aws_apigatewayv2_api.routing.id
@@ -42,7 +66,7 @@ resource "aws_apigatewayv2_stage" "routing" {
 
 
 {% for route in routing_routes %}
-  resource "aws_apigatewayv2_route" "route_{{route.id}}" {
+  resource "aws_apigatewayv2_route" "route_{{routing_id}}" {
     api_id    = aws_apigatewayv2_api.example.id
     route_key = "ANY {{route.path}}{proxy+}"
     target = "integrations/${aws_apigatewayv2_integration.{{service.name}}.id}"
