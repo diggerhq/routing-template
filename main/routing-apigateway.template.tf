fd4e6e76517264fd3bbbc4e18607aff25fc0e882
motatoes <moe.habib9@gmail.com>
2021-08-28 15:38:50 +0300

    use fn name

--- main/routing-apigateway.template.tf
+++ main/routing-apigateway.template.tf
@@ -60,7 +60,7 @@ resource "aws_apigatewayv2_stage" "routing" {
       connection_type    = "INTERNET"
     }
   {% elif service.service_type == "serverless" %}
-    data "aws_lambda_function" "{{service.name}}" {
+    data "aws_lambda_function" "{{service.function_name}}" {
       function_name = "{{service.function_name}}"
     }
 
