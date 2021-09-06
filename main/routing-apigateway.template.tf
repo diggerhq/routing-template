e991751fcddc8ad3c40573aaf69b9ec308d5fd8f
motatoes <moe.habib9@gmail.com>
2021-09-06 18:45:33 +0300

    sg fix

--- main/routing-apigateway.template.tf
+++ main/routing-apigateway.template.tf
@@ -31,7 +31,7 @@ resource "aws_apigatewayv2_api" "routing_{{routing_id}}" {
     from_port                = 80
     to_port                  = 80
     protocol                 = "tcp"
-    cidr_blocks = ["0.0.0.0"]
+    cidr_blocks = ["0.0.0.0/0"]
 
     security_group_id = aws_security_group.{{routing_id}}_sg.id
   }
