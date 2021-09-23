598d43a2d42b568b6f49931e381f70f1c4fae8c3
motatoes <moe.habib9@gmail.com>
2021-09-23 18:31:40 +0300

    include rest gateway

--- main/routing-apigateway.template.tf
+++ main/routing-apigateway.template.tf
@@ -1,166 +1,169 @@
+{% if http_api_gateway %}
 
-resource "aws_apigatewayv2_api" "routing_{{routing_id}}" {
-  name          = "${var.project_name}-${var.environment}-gateway"
-  protocol_type = "HTTP"
+  resource "aws_apigatewayv2_api" "routing_{{routing_id}}" {
+    name          = "${var.project_name}-${var.environment}-gateway"
+    protocol_type = "HTTP"
 
-  {% if routing_domain %}
-  cors_configuration {
-    allow_credentials  = true 
-    allow_headers      = ["*"]
-    allow_methods      = ["*"]
-    allow_origins      = "{{routing_domain}}"
-    # expose_headers     = ""
-    max_age            = 100
+    {% if routing_domain %}
+    cors_configuration {
+      allow_credentials  = true 
+      allow_headers      = ["*"]
+      allow_methods      = ["*"]
+      allow_origins      = "{{routing_domain}}"
+      # expose_headers     = ""
+      max_age            = 100
+    }
+    {% endif %}
+
+  }
+
+
+  {% if main_vpc_id %}
+    resource "aws_security_group" "{{routing_id}}_sg" {
+      name        = "${var.project_name}-${var.environment}-vpclink-sg"
+      description = "Allow TLS inbound traffic"
+      vpc_id      = "{{main_vpc_id}}"
+    }
+
+    # Rules for the LB (Targets the task SG)
+    resource "aws_security_group_rule" "nsg_task_ingress_rule" {
+      description              = "TLS VPCLink ingress"
+      type                     = "ingress"
+      from_port                = 80
+      to_port                  = 80
+      protocol                 = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+
+      security_group_id = aws_security_group.{{routing_id}}_sg.id
+    }
+
+    resource "aws_security_group_rule" "nsg_task_egress_rule" {
+      description = "TLS VPCLink egress"
+      type        = "egress"
+      from_port   = "0"
+      to_port     = "0"
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+
+      security_group_id = aws_security_group.{{routing_id}}_sg.id
+    }
+  {% endif %}
+
+
+  {% if routing_domain and routing_certificate %}
+  resource "aws_apigatewayv2_domain_name" "domain_{{routing_id}}" {
+    domain_name = "{{routing_domain}}"
+
+    domain_name_configuration {
+      certificate_arn = "{{routing_certificate}}"
+      endpoint_type   = "REGIONAL"
+      security_policy = "TLS_1_2"
+    }
   }
   {% endif %}
 
-}
-
-
-{% if main_vpc_id %}
-  resource "aws_security_group" "{{routing_id}}_sg" {
-    name        = "${var.project_name}-${var.environment}-vpclink-sg"
-    description = "Allow TLS inbound traffic"
-    vpc_id      = "{{main_vpc_id}}"
+  resource "aws_apigatewayv2_stage" "routing" {
+    api_id      = aws_apigatewayv2_api.routing_{{routing_id}}.id
+    name        = "$default"
+    auto_deploy = true
   }
 
-  # Rules for the LB (Targets the task SG)
-  resource "aws_security_group_rule" "nsg_task_ingress_rule" {
-    description              = "TLS VPCLink ingress"
-    type                     = "ingress"
-    from_port                = 80
-    to_port                  = 80
-    protocol                 = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
 
-    security_group_id = aws_security_group.{{routing_id}}_sg.id
-  }
+  {% for service in routing_services %}
+    {% if service.service_type == "container" and not service.internal %}
+      resource "aws_apigatewayv2_integration" "{{service.name}}" {
+        api_id           = aws_apigatewayv2_api.routing_{{routing_id}}.id
+        # credentials_arn  = aws_iam_role.example.arn
+        description      = "service {{service.name}} integration"
+        integration_type = "HTTP_PROXY"
+        integration_uri  = "http://{{service.lb_url}}"
 
-  resource "aws_security_group_rule" "nsg_task_egress_rule" {
-    description = "TLS VPCLink egress"
-    type        = "egress"
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+        integration_method = "ANY"
+        connection_type    = "INTERNET"
 
-    security_group_id = aws_security_group.{{routing_id}}_sg.id
-  }
-{% endif %}
+        request_parameters = {
+          "overwrite:path" = "$request.path.proxy"
+        }    
 
+      }
+    {% elif service.service_type == "container" and service.internal %}
 
-{% if routing_domain and routing_certificate %}
-resource "aws_apigatewayv2_domain_name" "domain_{{routing_id}}" {
-  domain_name = "{{routing_domain}}"
+      resource "aws_apigatewayv2_vpc_link" "{{service.name}}" {
+        name               = "${var.project_name}-{{service.name}}-vpclink"
+        security_group_ids = [aws_security_group.{{routing_id}}_sg.id]
+        subnet_ids         = ["{{public_subnet_a_id}}", "{{public_subnet_b_id}}"]
+      }
 
-  domain_name_configuration {
-    certificate_arn = "{{routing_certificate}}"
-    endpoint_type   = "REGIONAL"
-    security_policy = "TLS_1_2"
-  }
-}
-{% endif %}
+      resource "aws_apigatewayv2_integration" "{{service.name}}" {
+        api_id           = aws_apigatewayv2_api.routing_{{routing_id}}.id
+        # credentials_arn  = aws_iam_role.example.arn
+        description      = "Gateway integration for {{service.name}}"
+        integration_type = "HTTP_PROXY"
+        integration_uri  = "{{service.lb_http_listener_arn}}"
 
-resource "aws_apigatewayv2_stage" "routing" {
-  api_id      = aws_apigatewayv2_api.routing_{{routing_id}}.id
-  name        = "$default"
-  auto_deploy = true
-}
+        integration_method = "ANY"
+        connection_type    = "VPC_LINK"
+        connection_id      = aws_apigatewayv2_vpc_link.{{service.name}}.id
 
-
-{% for service in routing_services %}
-  {% if service.service_type == "container" and not service.internal %}
-    resource "aws_apigatewayv2_integration" "{{service.name}}" {
-      api_id           = aws_apigatewayv2_api.routing_{{routing_id}}.id
-      # credentials_arn  = aws_iam_role.example.arn
-      description      = "service {{service.name}} integration"
-      integration_type = "HTTP_PROXY"
-      integration_uri  = "http://{{service.lb_url}}"
-
-      integration_method = "ANY"
-      connection_type    = "INTERNET"
-
-      request_parameters = {
-        "overwrite:path" = "$request.path.proxy"
-      }    
-
-    }
-  {% elif service.service_type == "container" and service.internal %}
-
-    resource "aws_apigatewayv2_vpc_link" "{{service.name}}" {
-      name               = "${var.project_name}-{{service.name}}-vpclink"
-      security_group_ids = [aws_security_group.{{routing_id}}_sg.id]
-      subnet_ids         = ["{{public_subnet_a_id}}", "{{public_subnet_b_id}}"]
-    }
-
-    resource "aws_apigatewayv2_integration" "{{service.name}}" {
-      api_id           = aws_apigatewayv2_api.routing_{{routing_id}}.id
-      # credentials_arn  = aws_iam_role.example.arn
-      description      = "Gateway integration for {{service.name}}"
-      integration_type = "HTTP_PROXY"
-      integration_uri  = "{{service.lb_http_listener_arn}}"
-
-      integration_method = "ANY"
-      connection_type    = "VPC_LINK"
-      connection_id      = aws_apigatewayv2_vpc_link.{{service.name}}.id
-
-      # request_parameters = {
-      #   "overwrite:path" = "$request.path.proxy"
-      # }
-    }
+        # request_parameters = {
+        #   "overwrite:path" = "$request.path.proxy"
+        # }
+      }
 
 
-  {% elif service.service_type == "serverless" %}
-    data "aws_lambda_function" "{{service.name}}" {
-      function_name = "{{service.function_name}}"
-    }
+    {% elif service.service_type == "serverless" %}
+      data "aws_lambda_function" "{{service.name}}" {
+        function_name = "{{service.function_name}}"
+      }
 
-    resource "aws_apigatewayv2_integration" "{{service.name}}" {
-      api_id           = aws_apigatewayv2_api.routing_{{routing_id}}.id
-      integration_type = "AWS_PROXY"
+      resource "aws_apigatewayv2_integration" "{{service.name}}" {
+        api_id           = aws_apigatewayv2_api.routing_{{routing_id}}.id
+        integration_type = "AWS_PROXY"
 
-      connection_type           = "INTERNET"
-      description               = "Lambda {{service.name}}"
-      integration_method        = "POST"
-      integration_uri           = data.aws_lambda_function.{{service.name}}.invoke_arn
-      passthrough_behavior      = "WHEN_NO_MATCH"
+        connection_type           = "INTERNET"
+        description               = "Lambda {{service.name}}"
+        integration_method        = "POST"
+        integration_uri           = data.aws_lambda_function.{{service.name}}.invoke_arn
+        passthrough_behavior      = "WHEN_NO_MATCH"
 
-      request_parameters = {
-        "overwrite:path" = "$request.path.proxy"
+        request_parameters = {
+          "overwrite:path" = "$request.path.proxy"
+        }
+      }
+
+      # gateway permission
+      resource "aws_lambda_permission" "lambda_permission_{{service.name}}" {
+        statement_id  = "${var.project_name}${var.environment}{{service.name}}GWAPIInvoke"
+        action        = "lambda:InvokeFunction"
+        function_name = data.aws_lambda_function.{{service.name}}.function_name
+        principal     = "apigateway.amazonaws.com"
+
+        # The /*/*/* part allows invocation from any stage, method and resource path
+        # within API Gateway
+        source_arn = "${aws_apigatewayv2_api.routing_{{routing_id}}.execution_arn}/*/*/*"
+      }
+    {% endif %}
+  {% endfor %}
+
+
+  {% for route in routing_routes %}
+    resource "aws_apigatewayv2_route" "route_{{route.id}}" {
+      api_id    = aws_apigatewayv2_api.routing_{{routing_id}}.id
+      route_key = "ANY {{route.route_prefix}}{proxy+}"
+      target = "integrations/${aws_apigatewayv2_integration.{{route.service_name}}.id}"
+
+      lifecycle {
+        ignore_changes = [
+          authorization_scopes,
+          authorization_type,
+          authorizer_id,
+        ]
       }
     }
+  {% endfor %}
 
-    # gateway permission
-    resource "aws_lambda_permission" "lambda_permission_{{service.name}}" {
-      statement_id  = "${var.project_name}${var.environment}{{service.name}}GWAPIInvoke"
-      action        = "lambda:InvokeFunction"
-      function_name = data.aws_lambda_function.{{service.name}}.function_name
-      principal     = "apigateway.amazonaws.com"
-
-      # The /*/*/* part allows invocation from any stage, method and resource path
-      # within API Gateway
-      source_arn = "${aws_apigatewayv2_api.routing_{{routing_id}}.execution_arn}/*/*/*"
-    }
-  {% endif %}
-{% endfor %}
-
-
-{% for route in routing_routes %}
-  resource "aws_apigatewayv2_route" "route_{{route.id}}" {
-    api_id    = aws_apigatewayv2_api.routing_{{routing_id}}.id
-    route_key = "ANY {{route.route_prefix}}{proxy+}"
-    target = "integrations/${aws_apigatewayv2_integration.{{route.service_name}}.id}"
-
-    lifecycle {
-      ignore_changes = [
-        authorization_scopes,
-        authorization_type,
-        authorizer_id,
-      ]
-    }
+  output "lb_url" {
+    value = aws_apigatewayv2_api.routing_{{routing_id}}.api_endpoint
   }
-{% endfor %}
 
-output "lb_url" {
-  value = aws_apigatewayv2_api.routing_{{routing_id}}.api_endpoint
-}
+{% endif %}
---
+++ main/routing-rest_apigateway.template.tf
@@ -0,0 +1,73 @@
+{% if rest_api_gateway %}
+  resource "aws_api_gateway_rest_api" "routing_{{routing_id}}" {
+    name          = "${var.project_name}-${var.environment}-gateway"
+  }
+
+
+  resource "aws_api_gateway_deployment" "routing_{{routing_id}}" {
+    rest_api_id   = aws_api_gateway_rest_api.routing_{{routing_id}}.id
+  }
+
+  resource "aws_api_gateway_stage" "routing_{{routing_id}}" {
+    rest_api_id   = aws_api_gateway_rest_api.routing_{{routing_id}}.id
+    deployment_id = aws_api_gateway_deployment.routing_{{routing_id}}.id
+    stage_name        = "default"
+  }
+
+
+
+  {% for route in routing_routes %}
+    resource "aws_api_gateway_resource" "resource_{{route.id}}_parent" {
+      rest_api_id = aws_api_gateway_rest_api.routing_{{routing_id}}.id
+      parent_id   = aws_api_gateway_rest_api.routing_{{routing_id}}.root_resource_id
+      path_part   = ""
+    }
+
+    resource "aws_api_gateway_resource" "resource_{{route.id}}_child" {
+      rest_api_id = aws_api_gateway_rest_api.routing_{{routing_id}}.id
+      parent_id   = aws_api_gateway_resource.resource_{{route.id}}_parent.id
+      path_part   = "{{route.route_prefix}}"
+    }
+
+
+    {% if route.service.service_type == "container" and not route.service.internal %}
+
+      resource "aws_api_gateway_integration" "{{service.name}}_parent" {
+        rest_api_id = aws_api_gateway_rest_api.routing_{{routing_id}}.id
+        resource_id = aws_api_gateway_resource.resource_{{route.id}}_parent.id
+        http_method = "ANY"
+        type                    = "HTTP_PROXY"
+        integration_http_method = "ANY"
+        uri                     = "http://${route.service.lb_url}"
+        connection_type         = "INTERNET"
+        timeout_milliseconds    = 29000 # 50-29000
+      }
+
+      resource "aws_api_gateway_integration" "{{service.name}}_sub" {
+        rest_api_id = aws_api_gateway_rest_api.routing_{{routing_id}}.id
+        resource_id = aws_api_gateway_resource.resource_{{route.id}}_child.id
+        http_method = "ANY"
+        type                    = "HTTP_PROXY"
+        integration_http_method = "ANY"
+        uri                     = "http://${route.service.lb_url}/{proxy}"
+        connection_type         = "INTERNET"
+        timeout_milliseconds    = 29000 # 50-29000
+        cache_key_parameters = ["method.request.path.proxy"]
+        request_parameters = {
+          "integration.request.path.proxy" = "method.request.path.proxy"
+        }
+      }
+
+
+    {% elif route.service.service_type == "container" and route.service.internal %}
+
+    {% elif route.service.service_type == "serverless" %}
+
+
+  {% endfor %}
+
+  output "lb_url" {
+    value = aws_api_gateway_rest_api.routing_{{routing_id}}.api_endpoint
+  }
+
+{% endif %}
