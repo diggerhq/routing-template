fb142299f28df2ba9743f480571967a4121bae79
Alexey Skriptsov <veziak@yahoo.com>
2022-02-24 11:51:27 +0000

    Update routing-rest_apigateway.template.tf

--- main/routing-rest_apigateway.template.tf
+++ main/routing-rest_apigateway.template.tf
@@ -12,8 +12,8 @@
   resource "aws_api_gateway_deployment" "routing_{{routing_id}}" {
     rest_api_id   = aws_api_gateway_rest_api.routing_{{routing_id}}.id
     depends_on = [
-      aws_api_gateway_integration.integration_{{route.id}}_parent,
-      aws_api_gateway_integration.integration_{{route.id}}_child,
+      aws_api_gateway_integration.integration_{{routing_id}}_parent,
+      aws_api_gateway_integration.integration_{{routing_id}}_child,
       aws_api_gateway_rest_api.routing_{{routing_id}}
     ]
 
